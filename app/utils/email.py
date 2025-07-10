f6bb301f234fa860e8dd9e7da45132c951d18e67
Alexey <idolexey@yandex.ru>
2025-07-11 03:08:48 +0500

    Add sending email report

---
+++ app/utils/email.py
@@ -0,0 +1,56 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+from typing import Any
+
+from fastapi import HTTPException, status
+import pandas as pd
+from pydantic import BaseModel, EmailStr
+
+from core.config import settings
+
+
+class EmailAddress(BaseModel):
+    email: EmailStr
+
+
+def docs_to_html_table(docs: list[dict[str, Any]]) -> str:
+    docs_df = pd.DataFrame(docs)
+    docs_html_table = docs_df.to_html(index=False, border=1)
+    return docs_html_table
+
+
+def get_email_address(responsible: str) -> str:
+    if settings.debug == True:
+        email_address = settings.debug_email
+    else:
+        email_address = f"{responsible}@{settings.email.recievers_domain}"
+    return email_address
+
+
+def send_email(receiver_email: EmailAddress, content: str, email_subject: str) -> None:
+    msg = EmailMessage()
+    msg["Subject"] = email_subject
+    msg["From"] = settings.smtp.user
+    msg["To"] = receiver_email
+    msg.set_content("HTML content is not displayed", subtype="plain")
+    msg.add_alternative(content, subtype="html")
+
+    server = None
+
+    try:
+        context = ssl.create_default_context()
+        server = smtplib.SMTP(settings.smtp.host, settings.smtp.port)
+        server.starttls(context=context)
+        server.login(settings.smtp.user, settings.smtp.password)
+        server.send_message(msg)
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error while sending email",
+        ) from e
+
+    finally:
+        if server:
+            server.quit()
