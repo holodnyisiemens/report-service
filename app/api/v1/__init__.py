190f00e474355735269046b7c8ea370fb8a60712
Alexey <idolexey@yandex.ru>
2025-07-13 00:02:45 +0500

    Add router for responsible path

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from api.v1.hosts import router as hosts_router
+from api.v1.responsible import router as responsible_router
 from core.config import settings
 
 router = APIRouter()
@@ -8,4 +9,9 @@ router = APIRouter()
 router.include_router(
     hosts_router,
     prefix=settings.api.v1.hosts,
+)
+
+router.include_router(
+    responsible_router,
+    prefix=settings.api.v1.responsible,
 )
